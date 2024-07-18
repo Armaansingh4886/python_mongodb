3c85482210f71168acfa79d183c19a3777f8d562
armaansingh4886 <armaansingh4886@gmail.com>
2024-07-18 17:25:34 +0530

    upload data

--- apis.py
+++ apis.py
@@ -2,16 +2,18 @@ from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-
+from pymongo.errors import PyMongoError
 
 app = Flask(__name__)
 
 
 # app.config["MONGO_URI"] =  "mongodb+srv://<username>:<password>@cluster0.swa3ygw.mongodb.net/dataitems2?retryWrites=true&w=majority&appName=Cluster0"
-app.config["MONGO_URI"]="mongodb://localhost:27017/dataitems2"
-mongo = PyMongo(app)
-db_name = "dataitems2"
-collection_name = "data"
+# app.config["MONGO_URI"]="mongodb://localhost:27017/dataitems2"
+# mongo = PyMongo(app)
+# db_name = "dataitems2"
+# collection_name = "data"
+client = MongoClient('mongodb://localhost:27017/')
+db = client['dataitems2']
 
 # Example function to get you started
 def greet(name):
@@ -32,192 +34,200 @@ def safe_int(value, default=0):
 
 @app.route('/api/add_item', methods=['POST'])
 def add_item():
-    data = request.get_json()
-    distribution_channel = str(safe_int(data.get("distribution_channel_code", 0)))
-    location_code = str(safe_int(data.get("location_code", 0)))
-    sales_organization_code = str(safe_int(data.get("sales_orgranization_code", 0)))
-    item_no = str(safe_int(data.get("item_no", 0)))
-    customer = str(safe_int(data.get("customer_no", 0)))
-    customer_group = str(safe_int(data.get("customer_group", 0)))
-    customer_hierarchy = str(safe_int(data.get("customer_hierarchy", 0)))
+    try:
+        data = request.get_json()
+        distribution_channel = str(safe_int(data.get("distribution_channel_code", 0)))
+        location_code = str(safe_int(data.get("location_code", 0)))
+        sales_organization_code = str(safe_int(data.get("sales_organization_code", 0)))
+        item_no = str(safe_int(data.get("item_no", 0)))
+        customer = str(safe_int(data.get("customer_no", 0)))
+        customer_group = str(safe_int(data.get("customer_group", 0)))
+        customer_hierarchy = str(safe_int(data.get("customer_hierarchy", 0)))
 
-    if not sales_organization_code or not item_no or not data.get("Sales Price"):
-        return jsonify({"error": "Missing required parameters"}), 400
+        if not sales_organization_code or not item_no or not data.get("sales_price"):
+            return jsonify({"error": "Missing required parameters"}), 400
 
-    item_child = "nan"
-    if customer != "0": 
-        item_child = f"customer.{customer}"
-    elif customer_group != "0": 
-        item_child = f"customer_group.{customer_group}"
-    elif customer_hierarchy != "0":
-        item_child = f"customer_hierarchy.{customer_hierarchy}"
+        item_child = "nan"
+        if customer != "0": 
+            item_child = f"customer.{customer}"
+        elif customer_group != "0": 
+            item_child = f"customer_group.{customer_group}"
+        elif customer_hierarchy != "0":
+            item_child = f"customer_hierarchy.{customer_hierarchy}"
 
-    condition = {
-        "condition_type": data.get("condition_type", ""),
-        "starting_date": data.get("starting_cate", ""),
-        "unit_of_measure_code": data.get("unit_of_measure_code", ""),
-        "minimum_quantity": safe_float(data.get("minimum_quantity", 0.0)),
-        "priority": safe_int(data.get("priority", 0)),
-        "sales_price": safe_float(data.get("sales_price", 0.0)),
-        "ending_date": data.get("ending_date", ""),
-        "lower_limit": safe_float(data.get("lowe_limit", 0.0)),
-        "upper_limit": safe_float(data.get("upper_limit", 0.0)),
-        "currency": data.get("currency", ""),
-        "offer_article": safe_int(data.get("offer_artical", 0)),
-        "tax_percent": safe_float(data.get("tax_percent", 0.0)),
-        "price_with_tax": safe_float(data.get("price_with_tax", 0.0))
-    }
+        conditions = {
+            "condition_type": data.get("condition_type", ""),
+            "starting_date": data.get("starting_date", ""),
+            "unit_of_measure_code": data.get("unit_of_measure_code", ""),
+            "minimum_quantity": safe_float(data.get("minimum_quantity", 0.0)),
+            "priority": safe_int(data.get("priority", 0)),
+            "sales_price": safe_float(data.get("sales_price", 0.0)),
+            "ending_date": data.get("ending_date", ""),
+            "lower_limit": safe_float(data.get("lower_limit", 0.0)),
+            "upper_limit": safe_float(data.get("upper_limit", 0.0)),
+            "currency": data.get("currency", ""),
+            "offer_article": safe_int(data.get("offer_article", 0)),
+            "tax_percent": safe_float(data.get("tax_percent", 0.0)),
+            "price_with_tax": safe_float(data.get("price_with_tax", 0.0))
+        }
 
-    filter_query = {
-                "_id": ObjectId("66978dce76e719b74e07a02f")
-                }
-            
-    update_query = {
-                    "$push": {
-                            f"sales_organization_code.{sales_organization_code}.distribution_channels.{distribution_channel}.locations.{location_code}.items.{item_no}.{item_child}.conditions": condition
-                        }
-                   
-                }
-    result = mongo.db.data.update_one(filter_query, update_query)
-    print(result)
-    if result.matched_count > 0:
-        return jsonify({"success": True, "message": "Item added successfully"}), 200
-    else:
-        return jsonify({"error": "Unable to add item"}), 404
-    
+        document = {"_id":item_no}
+       
+        if "nan"==item_child:
+            document.update({"conditions": [conditions]})
+        else:
+            item_child_parts = item_child.split('.')
+            current_level = document
+            for part in item_child_parts:
+                if part not in current_level:
+                    current_level[part] = {}
+                current_level = current_level[part]
+            if "conditions" not in current_level:
+                current_level["conditions"] = []
+            current_level["conditions"].append(conditions)
+        collection = f"{sales_organization_code}.{distribution_channel}.{location_code}"
+        result = db[collection].insert_one(document)
+        print(result)
+        if result.inserted_id:
+            return jsonify({"success": True, "message": "Item added successfully"}), 200
+        else:
+            return jsonify({"error": "Unable to add item"}), 404
+    except PyMongoError as e:
+        return jsonify({'error': f'Unable to add item: {str(e)}'}), 500
 
 
 @app.route('/api/get_item', methods=['GET'])
 def get_item():
-    data = request.get_json()
+    try:
+        data = request.get_json()
     
-    distribution_channel = str(safe_int(data.get("distribution_channel_code", 0)))
-    location_code = str(safe_int(data.get("location_code", 0)))
-    sales_organization_code = str(safe_int(data.get("sales_orgranization_code", 0)))
-    item_no = str(safe_int(data.get("item_no", 0)))
-    customer = str(safe_int(data.get("customer_no", 0)))
-    customer_group = str(safe_int(data.get("customer_group", 0)))
-    customer_hierarchy = str(safe_int(data.get("customer_hierarchy", 0)))
+        distribution_channel = str(safe_int(data.get("distribution_channel_code", 0)))
+        location_code = str(safe_int(data.get("location_code", 0)))
+        sales_organization_code = str(safe_int(data.get("sales_organization_code", 0)))
+        item_no = str(safe_int(data.get("item_no", 0)))
+        customer = str(safe_int(data.get("customer_no", 0)))
+        customer_group = str(safe_int(data.get("customer_group", 0)))
+        customer_hierarchy = str(safe_int(data.get("customer_hierarchy", 0)))
 
-    if not sales_organization_code or not item_no :
-        return jsonify({"error": "Missing required parameters"}), 400
+        if not sales_organization_code or not item_no :
+            return jsonify({"error": "Missing required parameters"}), 400
 
-    item_child = "nan"
-    if customer != "0": 
-        item_child = f"customer.{customer}"
-    elif customer_group != "0": 
-        item_child = f"customer_group.{customer_group}"
-    elif customer_hierarchy != "0":
-        item_child = f"customer_hierarchy.{customer_hierarchy}"
+        item_child = "nan"
+        if customer != "0": 
+            item_child = f"customer.{customer}"
+        elif customer_group != "0": 
+            item_child = f"customer_group.{customer_group}"
+        elif customer_hierarchy != "0":
+            item_child = f"customer_hierarchy.{customer_hierarchy}"
     
-    path = f"sales_organization_code.{sales_organization_code}.distribution_channel.{distribution_channel}.location_code.{location_code}.item.{item_no}.{item_child}"
-    query = { path: { '$exists': True } }
-    projection = {f'{path}.conditions': 1, '_id': 0}
-    item = mongo.db.data.find_one(query,projection)
-    print(item != None)
-    if (item != None):
-        fetched_item = item['sales_organization_code'][sales_organization_code]['distribution_channel'][distribution_channel]['location_code'][location_code]['item'][item_no]
-
-        print("here")
-        if item_child == 'nan':
-            return jsonify({"message":fetched_item['nan']['conditions']})
+        query = {"_id": item_no}
+        projection = {f'{item_child}.conditions': 1, '_id': 0}
+        collection = f"{sales_organization_code}.{distribution_channel}.{location_code}"
+        item = db[collection].find_one(query,projection)
+        # print(item )
+        if (item != None):
+            fetched_item = item
+            print("here")
+            if item_child == 'nan':
+                return jsonify({"message":fetched_item['nan']['conditions']})
+            else:
+                item_childs = item_child.split(".")
+                return jsonify({'message':fetched_item[item_childs[0]][item_childs[1]]['conditions']})
         else:
-            item_childs = item_child.split(".")
-            return jsonify({'message':fetched_item[item_childs[0]][item_childs[1]]['conditions']})
-    else:
-        return jsonify({"error": "Unable to find item"}), 404
+            return jsonify({"error": "No item found"}), 404
+    except PyMongoError as e:
+        return jsonify({'error': f'Unable to get item: {str(e)}'}), 500
 
-
-@app.route('/api/update_item', methods=['POST'])
+@app.route('/api/update_item', methods=['PUT'])
 def update_item():
-    data = request.get_json()
-    distribution_channel = str(safe_int(data.get("distribution_channel_code", 0)))
-    location_code = str(safe_int(data.get("location_code", 0)))
-    sales_organization_code = str(safe_int(data.get("sales_orgranization_code", 0)))
-    item_no = str(safe_int(data.get("item_no", 0)))
-    customer = str(safe_int(data.get("customer_no", 0)))
-    customer_group = str(safe_int(data.get("customer_group", 0)))
-    customer_hierarchy = str(safe_int(data.get("customer_hierarchy", 0)))
+    try:
+        data = request.get_json()
+        distribution_channel = str(safe_int(data.get("distribution_channel_code", 0)))
+        location_code = str(safe_int(data.get("location_code", 0)))
+        sales_organization_code = str(safe_int(data.get("sales_organization_code", 0)))
+        item_no = str(safe_int(data.get("item_no", 0)))
+        customer = str(safe_int(data.get("customer_no", 0)))
+        customer_group = str(safe_int(data.get("customer_group", 0)))
+        customer_hierarchy = str(safe_int(data.get("customer_hierarchy", 0)))
 
-    if not sales_organization_code or not item_no or not data.get("sales_price"):
-        return jsonify({"error": "Missing required parameters"}), 400
+        if not sales_organization_code or not item_no :
+            return jsonify({"error": "Missing required parameters"}), 400
 
-    item_child = "nan"
-    if customer != "0": 
-        item_child = f"customer.{customer}"
-    elif customer_group != "0": 
-        item_child = f"customer_group.{customer_group}"
-    elif customer_hierarchy != "0":
-        item_child = f"customer_hierarchy.{customer_hierarchy}"
-    condition_keys = ["condition_type","currency","ending_date", "lower_limit", "minimum_quantity","offer_article","price_with_tax",
-      "priority",
-      "sales_organization_code",
-      "sales_price",
-      "starting_date",
-      "tax_percent",
-      "unit_of_measure_code",
-      "upper_limit"]
-    condition_obj={}
-    array_filter_obj={}
-    for key in condition_keys:
-        value = data.get(key)
-        if(value):
-            condition_obj[key]=value
-            array_filter_obj[f"elem.{key}"]=value
-    print(data.get("set"))
-    for key in data.get("set"):
-        print(key)
+        item_child = "nan"
+        if customer != "0": 
+            item_child = f"customer.{customer}"
+        elif customer_group != "0": 
+            item_child = f"customer_group.{customer_group}"
+        elif customer_hierarchy != "0":
+            item_child = f"customer_hierarchy.{customer_hierarchy}"
+        condition_keys = ["condition_type","currency","ending_date", "lower_limit", "minimum_quantity","offer_article","price_with_tax",
+        "priority",
+        "sales_price",
+        "starting_date",
+        "tax_percent",
+        "unit_of_measure_code",
+        "upper_limit"]
+        condition_obj={}
+        array_filter_obj={}
+        for key in condition_keys:
+            value = data.get(key)
+            if(value):
+                condition_obj[key]=value
+                array_filter_obj[f"elem.{key}"]=value
+        print(data.get("set"))
+        for key in data.get("set"):
+            print(key)
 
-    filter = {
-            f"sales_organization_code.{sales_organization_code}.distribution_channels.{distribution_channel}.locations.{location_code}.items.{item_no}.{item_child}.conditions" :{
-                '$elemMatch': condition_obj
-            }
-    }
-    path = f"sales_organization_code.{sales_organization_code}.distribution_channels.{distribution_channel}.locations.{location_code}.items.{item_no}.{item_child}.conditions.$[elem]"
-    setting_values = {}
-    setter = data.get("set")
-    for key in setter:
-        setting_values[f"{path}.{key}"]=setter[key]
-    update = {
-        '$set': setting_values
-    }
+        filter = {
+                "_id":item_no,
+                f"{item_child}.conditions":{"$elemMatch":condition_obj}
+        }
+        path = f"{item_child}.conditions.$[elem]"
+        setting_values = {}
+        setter = data.get("set")
+        for key in setter:
+            setting_values[f"{path}.{key}"]=setter[key]
+        update = {
+            '$set': setting_values
+        }
 
-    array_filters = [array_filter_obj]
-    
-    item = mongo.db.data.update_one(filter,update,array_filters=array_filters)
-    print(item)
-    if (item != None):
-        return jsonify({"message":"updated successfuly"})
+        array_filters = [array_filter_obj]
+        collection = f"{sales_organization_code}.{distribution_channel}.{location_code}"
+        print("asd",condition_obj)
+        item = db[collection].update_one(filter,update,array_filters=array_filters)
+        print(item)
+        if (item.modified_count >0):
+            return jsonify({"message":"updated successfuly"})
 
-    else:
-        return jsonify({"error": "error while updating item"}), 400
+        else:
+            return jsonify({"error": "no such item to update"}), 400
+    except PyMongoError as e:
+        return jsonify({'error': f'Unable to update item: {str(e)}'}), 500
     
 
 @app.route('/api/delete_item', methods=['DELETE'])
 def delete_item():
-    data = request.get_json()
-    item_no = str(safe_int(data.get("item_no", 0)))
-    distribution_channel = str(safe_int(data.get("distribution_channel_code", 0)))
-    location_code = str(safe_int(data.get("location_code", 0)))
-    sales_organization_code = str(safe_int(data.get("sales_orgranization_code", 0)))
-    
+    try:
+        data = request.get_json()
+        item_no = str(safe_int(data.get("item_no", 0)))
+        distribution_channel = str(safe_int(data.get("distribution_channel_code", 0)))
+        location_code = str(safe_int(data.get("location_code", 0)))
+        sales_organization_code = str(safe_int(data.get("sales_organization_code", 0)))
+        
 
-    filter = {
-        f'sales_organization_code.{sales_organization_code}.distribution_channels.{distribution_channel}.locations.{location_code}.items.{item_no}': {'$exists': True}
-    }
-      
-    update_query = {
-        '$unset': {
-            f'sales_organization_code.{sales_organization_code}.distribution_channels.{distribution_channel}.locations.{location_code}.items.{item_no}': ""
-        }
-    }
-    result = mongo.db.data.update_many(filter, update_query)
-    # result = mongo.db.data.find_one(filter)
-    print("result",result)
-    if result.matched_count > 0:
-        return jsonify({"success": True, "message": "Item deleted successfully"}), 200
-    else:
-        return jsonify({"error": "Unable to delete item"}), 404
+        filter = {
+                "_id":item_no
+            }
+        collection = f"{sales_organization_code}.{distribution_channel}.{location_code}"
+        result = db[collection].delete_one(filter)
+        # result = mongo.db.data.find_one(filter)
+        print("result",result)
+        if result.deleted_count > 0:
+            return jsonify({"success": True, "message": "Item deleted successfully"}), 200
+        else:
+            return jsonify({"error": "No such item to delete"}), 404
+    except PyMongoError as e:
+        return jsonify({'error': f'Unable to delete item: {str(e)}'}), 500
 
 
 if __name__ == '__main__':
--- pythonmongodb.py
+++ pythonmongodb.py
@@ -29,23 +29,15 @@ def safe_int(value, default=0):
 def process_csv_to_nested_dict(csv_file,mongo_url,db_name,collection_name,size):
     nested_dict = create_nested_dict()
     
-    # client = pymongo.MongoClient(mongo_url)
-    # db = client[db_name]
-    # collection = db[collection_name]
+    
     count =0
 
-    # collection.delete_many({})
-    
-    # new_document_id = ObjectId() 
-    # collection.insert_one({"_id": new_document_id})
-    EXTRA=0
     for chunk in pd.read_csv(csv_file,chunksize=size):
         count = count +1
         print(f"{size * count}")
         data = chunk.to_dict(orient="records")
         for row in data:
-            # EXTRA=EXTRA+1
-            # print(EXTRA)
+            
             distribution_channel = str(safe_int(row["Distribution Channel Code"]))
             location_code = str(safe_int(row["Location Code"]))
             sales_organization_code= str(safe_int(row["Sales Orgranization Code"]))
@@ -72,20 +64,11 @@ def process_csv_to_nested_dict(csv_file,mongo_url,db_name,collection_name,size):
                 "lower_limit": safe_float(row["Lower Limit"]),
                 "upper_limit": safe_float(row["Upper Limit"]),
                 "currency": row["Currency"],
-                "offer_article": safe_int(row["Offer Artical"]),
+                "offer_article": safe_int(row["Offer Article"]),
                 "tax_percent": safe_float(row["Tax Percent"]),
                 "price_with_tax": safe_float(row["Price with TAX"])
             }
-            # filter_query = {
-            #     "_id": new_document_id
-            #     }
-            
-            # update_query = {
-            #         "$push": {
-            #                 f"sales_organization_code.{sales_organization_code}.distribution_channels.{distribution_channel}.locations.{location_code}.items.{item_no}.{item_child}.conditions": condition
-            #             }
-                   
-            #     }
+         
 
             if item_child == "nan":
                 if "conditions" not in nested_dict["sales_organization_code"][sales_organization_code]["distribution_channel"][distribution_channel]["location_code"][location_code]["item"][item_no][item_child]:
@@ -97,9 +80,8 @@ def process_csv_to_nested_dict(csv_file,mongo_url,db_name,collection_name,size):
                     nested_dict["sales_organization_code"][sales_organization_code]["distribution_channel"][distribution_channel]["location_code"][location_code]["item"][item_no][item_child_parts[0]][item_child_parts[1]]["conditions"] = []
                 nested_dict["sales_organization_code"][sales_organization_code]["distribution_channel"][distribution_channel]["location_code"][location_code]["item"][item_no][item_child_parts[0]][item_child_parts[1]]["conditions"].append(condition)
 
-            # collection.update_one(filter_query, update_query)
-        # print((nested_dict))
-        return nested_dict
+            
+        # return nested_dict
     return nested_dict           
         
 def upload(data):
@@ -110,40 +92,16 @@ def upload(data):
     for collection_name in collections:
         db.drop_collection(collection_name)
     
-    # new_document_id = ObjectId() 
-    # collection.insert_one({"_id": new_document_id})
-
+   
     for key, value in data.items():
         print(f"Key: {key}")
         print(f"Value: {json.dumps(value)}\n")
-        key_list = key.split(".")
-        if key_list[1] == '0' and key_list[2] =='0':
-                document = {"_id": key}
-                document.update(value)
-                db["data00"].insert_one(document)
-        elif key_list[1] == '0' and key_list[2] =='1':
-            document = {"_id": key}
-            document.update(value)
-            db["data01"].insert_one(document)
-        elif key_list[1] == '1' and key_list[2] =='0':
-            document = {"_id": key}
-            document.update(value)
-            db["data10"].insert_one(document)
-        else:
-            document =  {"_id": key}
-            document.update(value)
-        
-            db["data"].insert_one(document)
-        # filter_query = {
-        #         "_id": new_document_id
-        #         }
-            
-        # update_query = {
-        #            "$set": {
-        #                     key: value
-        #                 }
-        #         }
-        # collection.update_one(filter_query, update_query)
+       
+        for itemno,itemdata in value.items():
+                    document = {"_id": itemno}
+                    document.update(itemdata)
+                    db[key].insert_one(document)
+      
 
 def save(data):
     filename = "./data.json"
@@ -156,10 +114,7 @@ def save(data):
 
 def create_key(path):
     return ".".join([
-        # f"{path[0]}.{path[1]}",
-        # f"{path[2]}.{path[3]}",
-        # f"{path[4]}.{path[5]}",
-        # f"{path[6]}"
+      
         f"{path[1]}.{path[3]}.{path[5]}"
     ])
 
@@ -168,22 +123,12 @@ def traverse_and_transform(d, path=None, result=None):
         path = []
     if result is None:
         result = {}
-    # if result10 is None:
-    #     result10 ={}
-
+ 
     for key, value in d.items():
         current_path = path + [key]
-        # if key =='0':
-        #     next_key =next(iter(value))
-        #     if (next_key =="item"):
-        #         obj_key = create_key(current_path)
-        #         result10[obj_key]= copy.deepcopy(value[next_key])
-        #     if next_key == "location_code":
-        #         obj_key = create_key(current_path)
-        #         result10[obj_key]= copy.deepcopy(value[next_key])
+        
         if key == 'item':
             obj_key = create_key(current_path)
-            # Deepcopy to ensure no reference issues
             result[obj_key] = copy.deepcopy(value)
         elif isinstance(value, defaultdict):
             traverse_and_transform(value, current_path, result)
@@ -194,7 +139,6 @@ csv_file = "./your_large_file.csv"
 # csv_file = "./data.csv"
 
 mongo_url="mongodb://localhost:27017"
-# mongo_url = "mongodb+srv://o7:pb12q4886@cluster0.swa3ygw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 db_name = "dataitems2"
 collection_name = "data"
 
@@ -204,15 +148,7 @@ new_item_add ={
     "asda":124312
 }
 
-data = process_csv_to_nested_dict(csv_file,mongo_url,db_name,collection_name,500000)
-# save(data)
-# print(data)
-# print(json.dumps(data["sales_orgamization_code"]["2100"]["distribution_channel"]["3"]["location_code"]["2120"]))
+data = process_csv_to_nested_dict(csv_file,mongo_url,db_name,collection_name,10000)
 transformed_result = traverse_and_transform(data)
 print(transformed_result)
 upload(transformed_result)
-# upload10(transformed_result[1])
-
-# for key, value in transformed_result.items():
-#     print(f"Key: {key}")
-#     print(f"Value: {json.dumps(value)}\n")
